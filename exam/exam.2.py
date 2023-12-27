3616e1cb2c3f20cc5b867ab3904f8d76d4b67358
imhojiakbar <152625715+imhojiakbar@users.noreply.github.com>
2023-12-27 21:04:23 +0500

    Update exam.2.py
    two methods

--- exam/exam.2.py
+++ exam/exam.2.py
@@ -16,3 +16,22 @@ if __name__ == '__main__':
     n = input(": ").split()
     answer(n)
 
+# def reverse_numbers(numbers):
+#     def reverse_number(number):
+#         reversed_num = int(str(number)[::-1])
+#         print(reversed_num)
+#     threadlar = []
+
+#     for number in numbers:
+#         thread = threading.Thread(target=reverse_number, args=(number,))
+#         thread.start()
+
+#         for thread in threadlar:
+#             thread.join()
+
+# if __name__ == '__main__':
+
+#     numbers = input("Enter: ")
+#     numbers = [int(num) for num in numbers.split()]
+#     reverse_numbers(numbers)
+
